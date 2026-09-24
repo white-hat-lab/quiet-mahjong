433b94fda5d39e9a659a2cc74473dead1dcef3a3
jyothi <naik0204@gmail.com>
2026-09-24 11:29:03 -0400

    Change the game background to slate blue

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'quiet-mahjong-offline-v6';
+const CACHE = 'quiet-mahjong-offline-v7';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
